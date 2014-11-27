34c99545e946533448fd0a4dfc170bda269e61f8
laufi <laufi_github@outlook.com>
2014-11-27 14:28:59 +0100

    Updates

--- controller.py
+++ controller.py
@@ -1,15 +1,17 @@
 import gui,stopwatch
-import time # !!!INTERIM!!! PLEASE REMOVE! WHEN NOT REMOVED HUNDREDS OF INFANTILFE DOGS WILL DIE ;-)
+import RPi.GPIO as GPIO
 class Controller (object):
+	inputpin = 25;
 	def __init__ (self):
 		self.home()
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(inputpin, GPIO.in)
 	def home (self):
 		self.gui = gui.Gui()
 	def newGame(self):
 		self.gui.startGameScreen()
 		uhr = stopwatch.Stopwatch()
-		uhr.start()
-		time.sleep(0.3) # !!!INTERIM!!! PLEASE REMOVE! WHEN NOT REMOVED HUNDREDS OF INFANTILFE DOGS WILL DIE ;-)
-		uhr.stop()
+		GPIO.add_event_detect(inputpin, GPIO.FALLING, callback = uhr.fehler(), bouncetime = 200)
+		sleep (8)
 		zeit = uhr.getTime()
 		print(zeit)
--- stopwatch.py
+++ stopwatch.py
@@ -2,8 +2,11 @@ import time
 class Stopwatch (object):
 	def start (self):
 		self.beginningTime = time.time()
+		self.fehler = 0
 	def stop (self):
 		self.endTime = time.time()
 		self.time = self.endTime - self.beginningTime
+	def fehler(self):
+		self.fehler = self.fehler + 1;
 	def getTime (self):
-		return(self.time)
+		return(self.time + self.fehler * 2)
