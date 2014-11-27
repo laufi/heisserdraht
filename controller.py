090f156b851c6bc3c1b84bd0c02198394efeec3d
laufi <laufi_github@outlook.com>
2014-11-27 15:15:14 +0100

    Bugfix

--- controller.py
+++ controller.py
@@ -1,20 +1,19 @@
 import gui,stopwatch
 import RPi.GPIO as GPIO
-class Controller (object):
-	def __init__ (self):
-		self.inputpin = 25;
-		self.home()
-		GPIO.setmode(GPIO.BCM)
-		GPIO.setup(self.inputpin, GPIO.IN)
-	def home (self):
-		self.gui = gui.Gui()
-	def fehler (self, channel):
-		self.fehlerzahl = self.fehlerzahl + 1
-	def newGame(self):
-		self.gui.startGameScreen()
-		uhr = stopwatch.Stopwatch()
-		self.fehlerzahl = 0
-		GPIO.add_event_detect(self.inputpin, GPIO.FALLING, callback = self.fehler(), bouncetime = 200)
-		sleep (8)
-		zeit = uhr.getTime() + fehlerzahl * 2
-		print(zeit)
+inputpin = 25;
+home()
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(inputpin, GPIO.IN)
+newGame()
+def home ():
+	gui = gui.Gui()
+def fehler (channel):
+	fehlerzahl = fehlerzahl + 1
+def newGame():
+	gui.startGameScreen()
+	uhr = stopwatch.Stopwatch()
+	fehlerzahl = 0
+	GPIO.add_event_detect(inputpin, GPIO.FALLING, callback = fehler(), bouncetime = 200)
+	sleep (8)
+	zeit = uhr.getTime() + fehlerzahl * 2
+	print(zeit)
