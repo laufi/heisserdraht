8d2c579ffa3f423f453581d6cf93ddf3e756f80b
laufi <laufi_github@outlook.com>
2014-11-27 15:12:27 +0100

    Bugfix

--- controller.py
+++ controller.py
@@ -8,11 +8,13 @@ class Controller (object):
 		GPIO.setup(self.inputpin, GPIO.IN)
 	def home (self):
 		self.gui = gui.Gui()
+	def fehler (self, channel):
+		self.fehlerzahl = self.fehlerzahl + 1
 	def newGame(self):
 		self.gui.startGameScreen()
 		uhr = stopwatch.Stopwatch()
-		fehlerzahl = 0
-		GPIO.add_event_detect(self.inputpin, GPIO.FALLING, callback = fehlerzahl = fehlerzahl + 1, bouncetime = 200)
+		self.fehlerzahl = 0
+		GPIO.add_event_detect(self.inputpin, GPIO.FALLING, callback = fehler(), bouncetime = 200)
 		sleep (8)
 		zeit = uhr.getTime() + fehlerzahl * 2
 		print(zeit)
