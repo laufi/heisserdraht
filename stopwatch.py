5a4b0ff0099f1066ffdb58cf79e2409d48d96f09
laufi <laufi_github@outlook.com>
2014-11-27 14:33:57 +0100

    Bugfix

--- stopwatch.py
+++ stopwatch.py
@@ -1,12 +1,12 @@
 import time
 class Stopwatch (object):
 	def start (self):
-		self.fehleranzahl = 0
 		self.beginningTime = time.time()
+		self.fehleranzahl = 0
 	def stop (self):
 		self.endTime = time.time()
 		self.time = self.endTime - self.beginningTime
-		self.fehleranzahl = self.fehleranzahl + 1;
 	def fehler(self):
-		return(self.time + self.fehleranzahl * 2)
+		self.fehleranzahl = self.fehleranzahl + 1;
 	def getTime (self):
+		return(self.time + self.fehleranzahl * 2)
